dad8c4d898efc1a6c351f1681e045786cf0e5909
gauravpandeyme143 <gp898170@gmail.com>
2024-12-18 14:21:10 +0530

    Add files via upload

---
+++ 1.2.c.c
@@ -0,0 +1,10 @@
+//farenhite and celcius//
+#include<stdio.h>
+int main(){
+    float f,c;
+    printf("enter the farenhite: ");
+    scanf("%d",&f);
+    c=(f-32)*5/9;
+    printf("celcius: %f",c);
+    return 0;
+}
