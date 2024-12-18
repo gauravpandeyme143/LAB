bc834ab2ee28ca8dbab997205ea1c6ae110808e5
gauravpandeyme143 <gp898170@gmail.com>
2024-12-18 14:09:09 +0530

    Add files via upload

---
+++ 1.1.c.c
@@ -0,0 +1,12 @@
+//code for adding 2 number//
+#include <stdio.h>
+int main(){
+    int a, b, sum;
+    printf("enter the value of a\n",a);
+    scanf("%d",&a);
+    printf("enter the value of b\n",b);
+    scanf("%d",&b);
+    sum=a+b;
+    printf("value of sum =%d",sum);
+    return 0;
+}
